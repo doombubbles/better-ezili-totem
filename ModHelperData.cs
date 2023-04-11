ef336c7f667aa85e5fe44184204098e71b7ee4de
doombubbles <obsidiantoolzg@gmail.com>
2023-04-10 19:51:33 -0700

    Update ModHelperData.cs

--- ModHelperData.cs
+++ ModHelperData.cs
@@ -2,6 +2,7 @@
 
 public static class ModHelperData
 {
+    public const string WorksOnVersion = "36.1";
     public const string Version = "1.1.2";
     public const string RepoOwner = "doombubbles";
     public const string RepoName = "better-ezili-totem";
@@ -9,4 +10,6 @@ public static class ModHelperData
 
     public const string Description =
         "Makes Ezili's Sacrifical Totem ability live up to its name... less. But for the purposes of fun, the ability no longer drains lives in order to use, so it's now usable on Impoppable and CHIMPS.";
+    
+    public const string Dependencies = "doombubbles/better-armor-piercing";
 }
